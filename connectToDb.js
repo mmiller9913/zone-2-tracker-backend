64b7220b57d8590590e3019a30f95913f31461e7
Matt Miller <mmiller9913@gmail.com>
2022-04-10 15:45:49 -0400

    edited db connection logic

--- connectToDb.js
+++ connectToDb.js
@@ -5,16 +5,16 @@ exports.connectToDb = () => {
     return new Promise((resolve, reject) => {
         const db = mysql.createConnection({
             // local
-            host: 'localhost',
-            user: 'root',
-            password: process.env.MYSQL_PASSWORD_LOCAL,
-            database: 'zone_2_tracker',
+            // host: 'localhost',
+            // user: 'root',
+            // password: process.env.MYSQL_PASSWORD_LOCAL,
+            // database: 'zone_2_tracker',
         
             //heroku
-            // host: 'us-cdbr-east-05.cleardb.net',
-            // user: 'b5e8c7635e0a90',
-            // password: process.env.MYSQL_PASSWORD_LOCAL,
-            // database: 'heroku_49aaaee3ed47db2',
+            host: 'us-cdbr-east-05.cleardb.net',
+            user: 'b5e8c7635e0a90',
+            password: process.env.MYSQL_PASSWORD_LOCAL,
+            database: 'heroku_49aaaee3ed47db2',
         });
         db.connect((err) => {
             if (err) {
